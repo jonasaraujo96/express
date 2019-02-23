56d6491a7994e92aa33dc92a862f93c493e69677
jonasaraujo96 <jonasaraujo96@gmail.com>
2019-02-23 10:37:00 -0300

    mongoose

--- config/database.js
+++ config/database.js
@@ -2,16 +2,24 @@ const mongoose = require('mongoose');
 
 module.exports=function(uri) {
         mongoose.connect(uri, {useNewUrlParser: true});
-}
+        
+        mongoose.connection.on('connected', function() {
+            console.log('Mongoose! conectado a ' + uri);
+        });
+        
+        mongoose.connection.on('disconnected', function() {
+            console.log('Mongoose! desconectado de ' + uri);
+        });
+        
+        mongoose.connection.on('error', function(erro) {
+            console.log('Mongoose! ERRO: ' + erro);
+        });
 
-mongoose.connection.on('conected', function() {
-    console.log('Mongoose! conectado a ' + uri);
-});
+        process.on('SIGINT', function () {
+            mongoose.connection.close(function () {
+                console.log('Mongoose! Desconectado pelo témino da aplicação');
 
-mongoose.connection.on('disconnected', function() {
-    console.log('Mongoose! desconectado de ' + uri);
-});
-
-mongoose.connection.on('error', function(erro) {
-    console.log('Mongoose! ERRO: ' + erro);
-});
+                process.exit(0);
+            });
+        });
+    }
